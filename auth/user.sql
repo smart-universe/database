8c8520be2b0d3ca7fd25efc4bc34f0d467c68df0
bharat148vishnubhatla@gmail.com <bharat148vishnubhatla@gmail.com>
2021-12-05 00:44:36 +0530

    Adding auth schema files

---
+++ auth/user.sql
@@ -0,0 +1,9 @@
+create table user(username varchar(40) not null, 
+last_name varchar(40),
+country varchar(40), 
+email_id varchar(60), 
+first_name varchar(40), 
+password text,
+ algotithm varchar(45),
+ primary key (username)
+ );
